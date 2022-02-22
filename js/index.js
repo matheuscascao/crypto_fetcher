8f82052c37944b440df95f8f532780b9ac5531f6
Matheus Cascão <matheusfranco23@hotmail.com>
2022-02-21 21:31:57 -0300

    Frontend and some functionalities
    Frontend has been improved with CSS and a more attractive layout.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@ async function getList(){
   dataList = await axios.get('https://api.coingecko.com/api/v3/exchanges');
 
   return dataList;
-
 }
 
 async function filterData(selectedData){
@@ -21,13 +20,32 @@ async function filterData(selectedData){
     };
   };
 
+  console.log(filteredObjects);
   return filteredObjects;
 };
 
 function appendElement(exchange){
   // Append elements on the DOM
-  let liCreate = window.document.createElement("li");
-  liCreate.innerHTML = `<p>${exchange.country} - ${exchange.name}</p>`;
+
+  let liCreate = window.document.createElement("a");
+  liCreate.classList.add("exchange_container")
+
+  // If there's a exchange in the given country, show it, otherwise...
+  if(exchange){
+    liCreate.innerHTML = `
+      <h3 class="exchange_items_header" href=${exchange.url}> <img src="${exchange.image}" alt="">${exchange.name}</h3>
+      <div class="exchange_infos">
+        <p>Country: <span class="info_highlight">${data_country.value}</span></p>
+        <p>Trading Volume: <span class="info_highlight">${parseInt(exchange.trade_volume_24h_btc)} BTCs</span></p>
+      </div>`;
+  } else if(exchange == false){
+    liCreate.innerHTML = `<p>${data_country.value} doesn't have any exchanges</p>`;
+  };
+
+  // liCreate.setAttribute('id', 'exchange_item_container');
+  liCreate.href = exchange.url;
+  liCreate.target = "_blank";
+
   listShown.appendChild(liCreate);
 };
 
@@ -35,13 +53,17 @@ async function mainShow(){
   // call the filterData() function and show it on the screen
   data = await filterData(data_country.value);
 
-  // shows the main infos on the console, for debug purposes
+  // shows the main info on the console, for debug purposes
   console.log("Country: " + data_country.value);
   console.log(data.length);
 
-  for(let q = 0; q < data.length; q++){
-    appendElement(data[q]);
-  };
+  if(data.length >= 1){
+    for(let q = 0; q < data.length; q++){
+      appendElement(data[q]);
+    };
+  } else{
+    appendElement(false);
+  }
 
 };
 
